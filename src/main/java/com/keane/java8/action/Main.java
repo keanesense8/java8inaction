0a1018b6299abccf2833b5719999a4f4fe747dc4
keanesense8 <tengjuny0123@gmail.com>
2019-01-12 14:31:43 +0800

    init

---
+++ src/main/java/com/keane/java8/action/Main.java
@@ -0,0 +1,8 @@
+package com.keane.java8.action;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/test/java/com/keane/java8/action/stream/StreamTest.java
@@ -0,0 +1,16 @@
+package com.keane.java8.action.stream;
+
+import org.junit.Test;
+
+import java.util.stream.Stream;
+
+
+public class StreamTest {
+
+    @Test
+    public void StreamIterateTest(){
+
+    }
+
+
+}
